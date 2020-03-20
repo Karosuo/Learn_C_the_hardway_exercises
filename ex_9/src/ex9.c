610512fbebe8e138f63c4e5505585de799c35aab
Rafael Karosuo <rafaelkarosuo@gmail.com>
2020-03-19 23:03:33 -0700

    Added continue to while loop so it runes forever even when the boolean comparison is correct

--- ex_9/src/ex9.c
+++ ex_9/src/ex9.c
@@ -5,7 +5,14 @@ int main(int argc, char * argv[])
 	int i = 25;
 	while(i > 0)
 	{
-		printf("%d\n", i);
+		printf("%d\n", i);		
+		
+		if( (i % 2) == 0 )
+		{
+			printf("Breaking...\n");
+			continue; // Run forever
+		}
+		
 		i--;
 	}
 	
